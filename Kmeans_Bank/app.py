b3f862f877ef5a9f40f5faf236f566da6b8ae1bb
Nathalia F. <79790966+Nathifer@users.noreply.github.com>
2024-11-10 14:42:11 +0100

    Update app.py

--- Kmeans_Bank/app.py
+++ Kmeans_Bank/app.py
@@ -1,51 +1,41 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Cargar el conjunto de datos
-@st.cache_data  # Esto asegura que el archivo se cargue solo una vez
+# Función para cargar el dataset desde GitHub
+@st.cache_data
 def load_data():
-    data = pd.read_csv('bank_dataset.csv')  # Asegúrate de que el archivo esté en la ruta correcta
+    url = "https://raw.githubusercontent.com/Nathifer/Project-Machine-Learning/main/Kmeans_Bank/bank_dataset.csv"
+    data = pd.read_csv(url)
     return data
 
-# Función para mostrar el dataset
-def show_data():
+# Función para mostrar los primeros datos
+def show_data(bank):
     st.write("### Datos del Banco")
-    st.write(bank.head())  # Muestra las primeras filas del dataset
+    st.write(bank.head())
 
 # Función para mostrar estadísticas descriptivas
-def show_statistics():
+def show_statistics(bank):
     st.write("### Estadísticas Descriptivas")
     st.write(bank.describe())
-
-# Función para mostrar un gráfico de dispersión
-def show_scatter_plot():
-    st.write("### Gráfico de Edad vs. Balance")
-    fig, ax = plt.subplots()
-    sns.scatterplot(data=bank, x='age', y='balance', ax=ax)
-    st.pyplot(fig)
 
 # Función principal para la interfaz de Streamlit
 def main():
     st.title('Análisis de Datos del Banco')
 
-    # Cargar datos
+    # Cargar los datos desde GitHub
     bank = load_data()
 
     # Crear una barra lateral para navegación
     option = st.sidebar.selectbox(
         'Selecciona una opción:',
-        ['Ver Datos', 'Estadísticas Descriptivas', 'Gráfico de Edad vs. Balance']
+        ['Ver Datos', 'Estadísticas Descriptivas']
     )
 
     # Mostrar los datos seleccionados
     if option == 'Ver Datos':
-        show_data()
+        show_data(bank)
     elif option == 'Estadísticas Descriptivas':
-        show_statistics()
-    elif option == 'Gráfico de Edad vs. Balance':
-        show_scatter_plot()
+        show_statistics(bank)
 
 if __name__ == '__main__':
     main()
